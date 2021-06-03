20303c536548f3ea0222d693452e58bd638f3ebe
Nahems <clement.nahmens@laplateforme.io>
2021-06-03 15:13:13 +0200

    barre de recherche afficher

--- view/layout.php
+++ view/layout.php
@@ -114,6 +114,9 @@ function remplaceID($nameA, $urlA)
                 </div>
             </a>
         </div>
+
+        <!-- barre de recherche -->
+
         <div id="header_lien">
             <div id="header_lien_grid1">
                 <form method="post">
@@ -127,10 +130,52 @@ function remplaceID($nameA, $urlA)
                 </form>
             </div>
             <?php
+            $recherche = isset($_GET['search']) ? $_GET['search'] : '';
+            if (isset($_GET['search'])) {
+                $data = $search->getSearch($recherche);
+            }
+            ?>
+            <!DOCTYPE html>
+            <html lang="fr">
+
+            <head>
+                <title>Recherche</title>
+                <meta charset="utf-8">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                <link rel="stylesheet" href="fontawesome/all.css">
+                <link rel="stylesheet" type="text/css" href="autocompletion.css">
+            </head>
+
+            <header>
+                <div id="header_div">
+                    <form id="header_form" method="POST">
+                        <div id="flfl">
+                            <input type="search" name="search" aria-label="recherche" id="searchNav" autocomplete="off">
+                            <div id="fl2">
+                                <div class="autocompletion" id="autocompletion"></div>
+                            </div>
+                        </div>
+                        <input type="submit" id="submitNav" value="Rechercher">
+                    </form>
+                </div>
+            </header>
+            <main id="mainmain">
+                <?php if (isset($data)) { ?>
+                    <div id="re_div">
+                        <span id="searchDisplay" class="searchDisplay">
+                            <?php echo $data; ?>
+                        </span>
+                    </div>
+                <?php } ?>
+            </main>
+            <?php
             if (isset($_SESSION['id'])) {
             ?>
+
+                <!-- bouton de deconnexion -->
+
                 <div id="header_lien_button">
-                    <button><a href="disconnect">Déconexion</a> </button>
+                    <button><a href="disconnect">Déconnexion</a> </button>
                 </div>
             <?php
             } else {
