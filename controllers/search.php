36457bc2f5b7320883a238a73d0a8595bd7ed580
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-06-07 14:37:49 +0200

    ajout clement

---
+++ controllers/search.php
@@ -0,0 +1,82 @@
+<?php
+
+class Search
+{
+
+    private $id;
+    private $nom;
+    private $db;
+
+    public function __construct($db)
+    {
+
+        $this->db = $db;
+    }
+
+    public function getSearch($recherche)
+    {
+
+        $connexion_db = $this->db->connectDb();
+
+        $q = $connexion_db->prepare("SELECT * FROM sports WHERE nom LIKE '%$recherche%'");
+        $q->execute();
+        $search_results = $q->fetchAll(PDO::FETCH_ASSOC);
+
+        $search = null;
+
+        if (!empty($search_results[0])) {
+
+            foreach ($search_results as $r) {
+
+                $search .= "<a href='element.php?id=" . $r['id'] . "'><p class='element'>" . $r['nom'] . "</p></a><br/>";
+            }
+
+            return $search;
+        } else {
+
+            return '<p class="resultNull">Aucun résultat</p>';
+        }
+    }
+
+    public function autoCompletion($recherche)
+    {
+
+        $connexion_db = $this->db->connectDb();
+
+        // la requete mysql
+        $q = $connexion_db->prepare("SELECT * FROM sports WHERE nom LIKE '%$recherche%' LIMIT 5");
+        $q->execute();
+        $search_results = $q->fetchAll(PDO::FETCH_ASSOC);
+
+        $search = null;
+
+        if (!empty($search_results[0])) {
+
+            foreach ($search_results as $r) {
+
+                $search .= "<a href='recherche.php?search=" . $r['nom'] . "'><p class='result'>" . $r['nom'] . "</p></a><br/>";
+            }
+
+            return $search;
+        } else {
+
+            return '<p class="resultNull">Aucun résultat</p>';
+        }
+    }
+
+    public function getRequestInfo($id)
+    {
+
+        $connexion_db = $this->db->connectDb();
+        $q = $connexion_db->prepare("SELECT * FROM sports WHERE id = $id ");
+        $q->execute();
+        $search_results = $q->fetch(PDO::FETCH_ASSOC);
+
+        if (!empty($search_results)) {
+
+            return $search_results;
+        } else {
+            echo 'Pas de résultat';
+        }
+    }
+}
