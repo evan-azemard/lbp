c57756b0d6478cb43b20f71d33e96cfb92cedd8a
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-06-07 14:39:28 +0200

    debug index

--- view/layout.php
+++ view/layout.php
@@ -119,44 +119,6 @@ require_once  ('library/fonctions.php');
                 $data = $search->getSearch($recherche);
             }
             ?>
-            <!DOCTYPE html>
-            <html lang="fr">
-
-            <head>
-                <title>Recherche</title>
-                <meta charset="utf-8">
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-                <link rel="stylesheet" href="fontawesome/all.css">
-                <link rel="stylesheet" type="text/css" href="autocompletion.css">
-            </head>
-
-            <header>
-                <div id="header_div">
-                    <form id="header_form" method="POST">
-                        <div id="flfl">
-                            <input type="search" name="search" aria-label="recherche" id="searchNav" autocomplete="off">
-                            <div id="fl2">
-                                <div class="autocompletion" id="autocompletion"></div>
-                            </div>
-                        </div>
-                        <input type="submit" id="submitNav" value="Rechercher">
-                    </form>
-                </div>
-            </header>
-            <main id="mainmain">
-                <?php if (isset($data)) { ?>
-                    <div id="re_div">
-                        <span id="searchDisplay" class="searchDisplay">
-                            <?php echo $data; ?>
-                        </span>
-                    </div>
-                <?php } ?>
-            </main>
-            <?php
-            if (isset($_SESSION['id'])) {
-            ?>
-
-
 
     <!--Main-->
 
