4535747640fa2821c5c810b96a6f0add01a1a600
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-06-07 15:43:03 +0200

    tentative rewrite

--- view/produit.php
+++ view/produit.php
@@ -100,7 +100,7 @@ if (isset($_POST['ppan'])){
                                     </form>
                                     <p><?= $article['prix_article']?>€</p>
                                     <p><?= $article['code']?></p>
-                                    <button><a href="contact?id_produit=<?= $article['id_produit']?>&?id_vendeur=<?= $article['id_vendeur']?>&?id_client=<?= $_SESSION['id']?>">Contacter</a></button>
+                                    <button><a href="contact?id_produit=<?= $article['id_produit']?>&?id_vendeur=<?= $article['id_vendeur']?>">Contacter</a></button>
                                 </div>
                             </article>
                         <?php } ?>
