4b8513db2836923379e6dfe3d8a3732a117ebc2b
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-06-08 15:36:01 +0200

    Le vendeur peut répondre au client , la reponse est automatiquement envoyé par mail, La fonction ne peut pas marcher en local .

--- controllers/voir.php
+++ controllers/voir.php
@@ -37,44 +37,69 @@ function voir(){
                 {
 
 
+                    if (isset($_POST['submit'])){
+
+                        if (!empty($_POST['resum']))
+                        {
+
+                            echo $selusers['email'];
+
+                            $message = 'Message de la part du vendeur  Smart Your Future
+                          Votre message : ' . $relproduits['resum'] . '
+                          
+                          Reponse : ' . $_POST['reponse']. '';
+
+                            /*La fonction ne peut pas marcher en local*/
+                            $retour = mail($selusers['email'],'Reponse Vendeur SYF', $message);
+                            if($retour) {
+                                ?><script> alert("Votre message a bien été envoyé.") </script>
+                                <?php
+                            }
+
+                        }
+
+
+
+
+                    }
 
                     ?>
 
                     <div class="admin_div">
-        <div class="admin_cont1">
-            <div class="admin_cont1_text">
-                <div class="admin_label">
-                    <p>Nom : <span class="admin_gray"><?= $selusers['nom'] ?></span> </p>
-                </div>
-                <div class="admin_label">
-                    <p>Prénom : <span class="admin_gray"><?= $selusers['prenom'] ?></span></p>
-                </div>
-                <div class="admin_label">
-                    <p>Id produit : <span class="admin_gray">  <?= $relproduits['id_produit'] ?></span> </p>
-                </div>
-                <div class="admin_label">
-                    <p>Nom du produit : <span class="admin_gray"><?= $relproduits['nom_model'] ?></span> </p>
-                </div>
-            </div>
-            <div class="admin_cont1_textarea">
-                <div class="admin_cont_texta">
-                    <p style="display: flex">
-                       <?= $relproduits['resum'] ?>
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="traimoyen_admin"></div>
-        <div class="admin_cont2">
-            <form class="admin_form">
-                <textarea aria-label="textarea" class="admin_textarea" placeholder="Méssage de réponse[...]"></textarea>
-                <div class="admin_button">
-                    <input type="submit" class="button" name="envoyer" value="Envoyer">
-<!--                    <input type="submit" class="button" name="signaler" value="Signaler">
--->                </div>
-            </form>
-        </div>
-    </div>
+                        <div class="admin_cont1">
+                            <div class="admin_cont1_text">
+                                <div class="admin_label">
+                                    <p>Nom : <span class="admin_gray"><?= $selusers['nom'] ?></span> </p>
+                                </div>
+                                <div class="admin_label">
+                                    <p>Prénom : <span class="admin_gray"><?= $selusers['prenom'] ?></span></p>
+                                </div>
+                                <div class="admin_label">
+                                    <p>Id produit : <span class="admin_gray">  <?= $relproduits['id_produit'] ?></span> </p>
+                                </div>
+                                <div class="admin_label">
+                                    <p>Nom du produit : <span class="admin_gray"><?= $relproduits['nom_model'] ?></span> </p>
+                                </div>
+                            </div>
+                            <div class="admin_cont1_textarea">
+                                <div class="admin_cont_texta">
+                                    <p style="display: flex">
+                                        <?= $relproduits['resum'] ?>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="traimoyen_admin"></div>
+                        <div class="admin_cont2">
+                            <form class="admin_form" method="post">
+                                <textarea aria-label="textarea" name="reponse" class="admin_textarea" placeholder="Méssage de réponse[...]"></textarea>
+                                <div class="admin_button">
+                                    <input type="submit" class="button" name="submit" value="Envoyer">
+                                    <!--                    <input type="submit" class="button" name="signaler" value="Signaler">
+                                    -->                </div>
+                            </form>
+                        </div>
+                    </div>
 
                     <?php
 
