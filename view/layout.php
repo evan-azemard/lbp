9051e0c836cb84e01fe5a7fc4c8dcf871e479659
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-06-09 14:29:33 +0200

    innitialisation des fichiers de responsive

--- view/layout.php
+++ view/layout.php
@@ -11,7 +11,11 @@ require_once('library/fonctions.php');
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="https://th.bing.com/th/id/R8b9399dc465e6ade5afd8001ed17a012?rik=c%2fW8QQUaPwl%2fPA&riu=http%3a%2f%2fimages.clipartpanda.com%2fphone-icon-phone-512.png&ehk=nxS%2fIZTzi%2bOuaWlzTAP5khMCqyHyy%2fG0FlBqvfFhBhI%3d&risl=&pid=ImgRaw">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/grand.css">
+    <link rel="stylesheet" type="text/css" href="css/moyen.css">
+    <link rel="stylesheet" type="text/css" href="css/tablette.css">
+    <link rel="stylesheet" type="text/css" href="css/phablette.css">
+    <link rel="stylesheet" type="text/css" href="css/smartphone.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Sanchez&display=swap" rel="stylesheet">
     <script src="https://use.fontawesome.com/560ce3d2ed.js"></script>
