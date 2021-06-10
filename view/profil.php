696d15f0238b7865275c4ad57d7bbb19c8828788
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-06-10 05:24:56 +0200

    Page profil Responsive fait

--- view/profil.php
+++ view/profil.php
@@ -34,51 +34,51 @@ if (isset($_POST["submit"])) {
             <article id="register_form_article2">
                 <div class="register_form_contient">
                     <div class="register_labput">
-                        <label for="pseudo"> Pseudo</label>
-                        <input type="text" minlength="4" placeholder="<?= $_SESSION['pseudo'] ?>" name="pseudo" maxlength="12" id="pseudo">
+                        <label class="profil_invi" for="pseudo"> Pseudo</label>
+                        <input type="text" minlength="4" placeholder="<?= $_SESSION['pseudo'] ?> (pseudo)" name="pseudo" maxlength="12" id="pseudo">
                     </div>
                     <div class="register_labput">
-                        <label for="téléphone">Téléphone</label>
+                        <label class="profil_invi" for="téléphone">Téléphone</label>
                         <div id="telregister">
                             <p>+33</p>
                         </div>
-                        <input type="number" name="tel" placeholder="<?= $_SESSION['tel'] ?>" maxlength="9" minlength="9" id="téléphone">
+                        <input type="number" name="tel" placeholder="<?= $_SESSION['tel'] ?>(téléphone)" maxlength="9" minlength="9" id="téléphone">
                     </div>
                 </div>
                 <div class="register_form_contient">
                     <div class="register_labput">
-                        <label for="password">Mot de passe</label>
+                        <label class="profil_invi" for="password">Mot de passe</label>
                         <input type="password" placeholder="password" name="password" minlength="12" maxlength="40" id="password">
                     </div>
                     <div class="register_labput">
-                        <label for="email">Email</label>
-                        <input type="email" minlength="9" placeholder="<?= $_SESSION['email'] ?>" name="email" maxlength="35" id="email">
+                        <label class="profil_invi" for="email">Email</label>
+                        <input type="email" minlength="9" placeholder="<?= $_SESSION['email'] ?>(email)" name="email" maxlength="35" id="email">
                     </div>
                 </div>
                 <div class="register_form_contient">
                     <div class="register_labput">
-                        <label for="confirme_password">Confirmation du mot de passe</label>
+                        <label class="profil_invi" for="confirme_password">Confirmation du mot de passe</label>
                         <input type="password" placeholder="confirmer password" name="r_password" minlength="12" maxlength="40" id="confirme_password">
                     </div>
                     <div class="register_labput">
-                        <label for="age">Âge</label>
-                        <input type="number" name="age" placeholder="<?= $_SESSION['age'] ?>" minlength="13" maxlength="115" id="age">
+                        <label class="profil_invi" for="age">Âge</label>
+                        <input type="number" name="age" placeholder="<?= $_SESSION['age'] ?>(age)" minlength="13" maxlength="115" id="age">
                     </div>
                 </div>
                 <div class="register_form_contient">
                     <div class="register_labput">
-                        <label for="prenom">Prénom</label>
-                        <input type="text" name="prenom" placeholder="<?= $_SESSION['prenom'] ?>" maxlength="12" min="3" id="prenom">
+                        <label class="profil_invi" for="prenom">Prénom</label>
+                        <input type="text" name="prenom" placeholder="<?= $_SESSION['prenom'] ?>(prénom)" maxlength="12" min="3" id="prenom">
                     </div>
                     <div class="register_labput register_labput_login">
-                        <label for="adresse">Nouvelle adresse complète</label>
-                        <input type="text" name="adresse" placeholder="<?= $_SESSION['adresse'] ?>" minlength="20" maxlength="80" id="adresse">
+                        <label class="profil_invi" for="adresse">Nouvelle adresse complète</label>
+                        <input type="text" name="adresse" placeholder="<?= $_SESSION['adresse'] ?>(adresse)" minlength="20" maxlength="80" id="adresse">
                     </div>
                 </div>
                 <div class="register_form_contient">
                     <div class="register_labput">
-                        <label for="nom">Nom</label>
-                        <input type="text" name="nom" placeholder="<?= $_SESSION['nom'] ?>" maxlength="15" min="3" id="nom">
+                        <label class="profil_invi" for="nom">Nom</label>
+                        <input type="text" name="nom" placeholder="<?= $_SESSION['nom'] ?>(nom)" maxlength="15" min="3" id="nom">
                     </div>
                 </div>
             </article>
