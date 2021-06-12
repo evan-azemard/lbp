6b65db56ac9a85cc002c92e9dc02c01bfd065052
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-06-12 14:25:30 +0200

    Page gérant  Responsive fait

--- view/admin.php
+++ view/admin.php
@@ -8,41 +8,39 @@ if ($_SESSION['rank'] < 3) {
 
 ?>
 <main id="admin_main">
-    <div class="admin_div">
-        <div class="admin_cont1">
-            <div class="admin_cont1_text">
-                <div class="admin_label">
-                    <p>Nom : Exemple<span class="admin_gray"></span> </p>
-                </div>
-                <div class="admin_label">
-                    <p>Prénom : Exemple<span class="admin_gray"></span></p>
-                </div>
-                <div class="admin_label">
-                    <p>Id produit : Exemple<span class="admin_gray"></span> </p>
-                </div>
-                <div class="admin_label">
-                    <p>Email : Exemple<span class="admin_gray"></span> </p>
-                </div>
-            </div>
-            <div class="admin_cont1_textarea">
-                <div class="admin_cont_texta">
-                    <p>
-                        ExempleExempleExempleExempleExempleExempleExempleExempleExempleExemple
-                        ExempleExempleExempleExempleExempleExempleExempleExempleExemple
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="traimoyen_admin"></div>
-        <div class="admin_cont2">
-            <form class="admin_form">
-                <textarea aria-label="textarea" class="admin_textarea" placeholder="Méssage de réponse[...]"></textarea>
-                <div class="admin_button">
-                    <input type="submit" class="button" name="repondre" value="Lui répondre">
-                    <input type="submit" class="button" name="envoyer" value="Envoyer">
-                    <input type="submit" class="button" name="signaler" value="Signaler">
-                </div>
-            </form>
-        </div>
-    </div>
+   <div class="admin_div">
+                        <div class="admin_cont1">
+                            <div class="admin_cont1_text">
+                                <div class="admin_label">
+                                    <p>Nom : <span class="admin_gray"></span> </p>
+                                </div>
+                                <div class="admin_label">
+                                    <p>Prénom : <span class="admin_gray"></span></p>
+                                </div>
+                                <div class="admin_label">
+                                    <p>Id produit : <span class="admin_gray"> </span> </p>
+                                </div>
+                                <div class="admin_label">
+                                    <p>Nom du produit : <span class="admin_gray"></span> </p>
+                                </div>
+                            </div>
+                            <div class="admin_cont1_textarea">
+                                <div class="admin_cont_texta">
+                                    <p style="display: flex">
+
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="traimoyen_admin"></div>
+                        <div class="admin_cont2">
+                            <form class="admin_form" method="post">
+                                <textarea aria-label="textarea" name="reponse" class="admin_textarea" placeholder="Méssage de réponse[...]"></textarea>
+                                <div class="admin_button">
+                                    <input type="submit" class="button" name="submit" value="Envoyer">
+                                    <!--                    <input type="submit" class="button" name="signaler" value="Signaler">
+                                    -->                </div>
+                            </form>
+                        </div>
+                    </div>
 </main>
