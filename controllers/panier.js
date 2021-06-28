320d4bc9ffd6a46340bfac2aad77bdad7d3dd81a
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-06-28 20:57:27 +0200

    tentative popup

--- controllers/panier.js
+++ controllers/panier.js
@@ -1,21 +1,27 @@
-function panier() {
+/*function panier() {
+
     let article = document.getElementById("panier_section");
     let container = document.createElement("div");
+    let form = document.createElement("form");
     let paragraphe = document.createElement("p");
     let br = document.createElement("br");
+    let inputid = document.getElementsByClassName('idproduitpanier');*/
 
+
+/*
     let form = document.getElementById("formpanier");
-    let submit = document.createElement('input');
+*/
+/*    let submit = document.createElement('input');
 
     form.method = "post";
 
     let text = "voulez vous vraiment supprimer cette article de votre panier ?";
     submit.type = "submit";
     submit.value = "Confirmer";
-    submit.name = "Supprimer";
+    submit.name = "Confirmer";
     submit.className = "button_panier3";
-    container.className =  "panier_div";
-    paragraphe.className =  "panier_p";
+    container.className = "panier_div";
+    paragraphe.className = "panier_p";
 
     article.appendChild(container);
     paragraphe.append(text);
@@ -23,16 +29,37 @@ function panier() {
     container.appendChild(br);
     container.appendChild(form);
     form.appendChild(submit);
+    form.appendChild(inputid);*/
 
-       let but = document.querySelector('.button_panier');
-   but.remove();
+/*
+    container.appendChild(form);
+*/
+/*
+    form.appendChild(submit);
+*/
+
+/*
+    but = document.querySelectorAll('.button_panier');
+*/
+/*
+    but.forEach(
+  (el) => {
+    but.addEventListener('click', () => {
+      but.length > 1 && but.shift() // <- on retire le premier element s<il y en a plus que 1
+        but.remove()
+
+    })
+  }
+)*/
+
+/*
+}*/
+
+/*let but = document.querySelectorAll('.button_panier');
+but.forEach((e) => e.addEventListener('click', () => panier()))*/
 
 
 
-}
-
-let but = document.querySelectorAll('.button_panier');
-but.forEach((e) => e.addEventListener('click', () => panier()))
 
 
 
